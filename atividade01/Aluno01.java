1c94bfc29213534cce291476125ae7cb530a86a7
MAAGG <maysa_ferraresi@hotmail.com>
2015-04-10 21:27:02 -0300

    Entrega Atividade01

---
+++ atividade01/Aluno01.java
@@ -0,0 +1,23 @@
+class Aluno01 {
+	private string nome;
+	private int idade;
+	
+	public void SetNome (string nome) {
+		this.nome=nome;
+	public String getNome() {
+		return nome;
+	public void SetIdade (string idade) {
+		this.idade=idade;
+	public String getIdade(){
+		return idade;
+	}
+	}
+	}
+	}
+	public static void main (String [] args) {
+Aluno a = new Aluno ();
+a.setNome("Maysa");
+a.setIdade(22);
+
+System.out.Printl("Nome: "+p.getNome()+;", Idade:"+p.get.Idade());
+	}
